768d55ae89fed09b7291cfb25d25e88e0ee92c43
Emre Yilmaz <mail@emreyilmaz.me>
2018-04-11 23:05:46 +0300

    Initial commit

---
+++ tests.py
@@ -0,0 +1,29 @@
+import unittest
+
+from botchecker.checker import check
+
+
+class BotCheckerTests(unittest.TestCase):
+
+    def test_check_false(self):
+        is_bot, bot = check("emrebeyler")
+        self.assertEqual(is_bot, False)
+
+    def test_check_true(self):
+        is_bot, bot = check("buildawhale")
+        self.assertEqual(is_bot, True)
+
+    def test_check_bot_type(self):
+        is_bot, bot = check("jerrybanfield")
+        self.assertEqual(is_bot, True)
+        self.assertEqual(bot.type, 'bid_bot')
+
+        is_bot, bot = check("minnowbooster")
+        self.assertEqual(is_bot, True)
+        self.assertEqual(bot.type, 'other_bot')
+
+    def test_check_bot_account(self):
+        is_bot, bot = check("postpromoter")
+        self.assertEqual(bot.account, "postpromoter")
+
+
